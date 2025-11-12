5996a131f43ea14031649ecd48fa1bfaaa9af41f
william-uni <2661302@dundee.ac.uk>
2025-11-12 15:59:50 +0000

    javascript starter

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+function loadPatients() {
+  return JSON.parse(localStorage.getItem('patients') || '[]');
+}
+
+function savePatients(list) {
+  localStorage.setItem('patients', JSON.stringify(list));
+  renderPatients();
+  renderStats();
+}
